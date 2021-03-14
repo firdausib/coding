7a04037f6402f9ef0c91262b2a30184b310cdc22
firdausib <fikhwanulbaihaqi@gmail.com>
2021-03-15 02:03:34 +0700

    proyek

---
+++ coding.java
@@ -0,0 +1,17 @@
+public class kalkulator {
+	public static void main (String args []){
+		int a = 2, b=1;
+		
+		int tambah = a+b;
+		int kurang = a-b;
+		int kali = a*b;
+		int bagi =a/b;
+		int mod = a%b;
+		System.out.println("penjumlahan :" +tambah);
+		System.out.println("pengurangan :"+kurang);
+		System.out.println("perkalian "+kali);
+		System.out.println("pembagian :"+bagi);
+		System.out.println("modulus : "+modulus);
+	}
+
+}
